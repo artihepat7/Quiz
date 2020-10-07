16bea46f65cff0fdefd6a2c5f57ef117aae7b7d8
artihepat7 <hepatarti@gmail.com>
2020-10-07 13:36:19 +0530

    Quiz

---
+++ src/API/QuestionsQuiz.js
@@ -0,0 +1,47 @@
+export const QuestionsQuiz = [
+  {
+    questionText: "What is the capital of India?",
+    answerOptions: [
+      { answerText: "Maharashtra", isCorrect: false },
+      { answerText: "Punjab", isCorrect: false },
+      { answerText: "Delhi", isCorrect: true },
+      { answerText: "Mumbai", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Who is CEO of Google?",
+    answerOptions: [
+      { answerText: "Jeff Bezos", isCorrect: false },
+      { answerText: "Sundar Pichai", isCorrect: true },
+      { answerText: "Bill Gates", isCorrect: false },
+      { answerText: "Tony Stark", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "The iPhone was created by which company?",
+    answerOptions: [
+      { answerText: "Apple", isCorrect: true },
+      { answerText: "Intel", isCorrect: false },
+      { answerText: "Amazon", isCorrect: false },
+      { answerText: "Microsoft", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is the capital of Maharashtra?",
+    answerOptions: [
+      { answerText: "Delhi", isCorrect: false },
+      { answerText: "Punjab", isCorrect: false },
+      { answerText: "Pune", isCorrect: false },
+      { answerText: "Mumbai", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "The Sangai Festival is celebrated in __:",
+    answerOptions: [
+      { answerText: "Assam", isCorrect: false },
+      { answerText: "Manipur", isCorrect: true },
+      { answerText: "Mizoram", isCorrect: false },
+      { answerText: "Nagaland", isCorrect: false },
+    ],
+  },
+];
